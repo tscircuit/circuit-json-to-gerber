3b6b552b6d40c50ba1664f34a3090150f18bfc7a
Abdoaslam Allawlabi <abdaslam13.2.2001@gmail.com>
2025-09-19 18:30:06 +0200

    Feat: supported Pill and circle holes with offset + added support for rotated pill holes + Refactor the gerber SVG snapshot logic  (#58)
    * patch
    
    * patch

--- src/excellon-drill/commands/G85.ts
+++ src/excellon-drill/commands/G85.ts
@@ -5,6 +5,9 @@ export const G85 = defineExcellonDrillCommand({
   command_code: "G85",
   schema: z.object({
     command_code: z.literal("G85").default("G85"),
+    x: z.number(),
+    y: z.number(),
+    width: z.number(), // slot width = tool diameter
   }),
-  stringify: () => "G85",
+  stringify: ({ x, y }) => `G85X${x.toFixed(3)}Y${y.toFixed(3)}`,
 })
