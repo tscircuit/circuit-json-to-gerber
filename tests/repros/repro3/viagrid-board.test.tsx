d939196f4f760967887b9ad653bcc4a1ec4ac5e4
Rishabh Gupta <38923768+imrishabh18@users.noreply.github.com>
2025-11-08 02:09:15 +0530

    test: pass the excellon drill for the top layer to show the holes under the copper pour (#64)

--- tests/repros/repro3/viagrid-board.test.tsx
+++ tests/repros/repro3/viagrid-board.test.tsx
@@ -1,12 +1,32 @@
 import { test, expect } from "bun:test"
 import { convertSoupToGerberCommands } from "src/gerber/convert-soup-to-gerber-commands"
 import { stringifyGerberCommandLayers } from "src/gerber/stringify-gerber"
+import {
+  convertSoupToExcellonDrillCommands,
+  stringifyExcellonDrill,
+} from "src/excellon-drill"
 import viagridCircuitJson from "./assets/viagrid-circuit-json.json"
 
 test("Generate viagrid board", async () => {
   const gerber_cmds = convertSoupToGerberCommands(viagridCircuitJson as any)
+  const excellon_drill_cmds = convertSoupToExcellonDrillCommands({
+    circuitJson: viagridCircuitJson as any,
+    is_plated: true,
+  })
+  const excellon_drill_cmds_unplated = convertSoupToExcellonDrillCommands({
+    circuitJson: viagridCircuitJson as any,
+    is_plated: false,
+  })
 
   const gerberOutput = stringifyGerberCommandLayers(gerber_cmds)
+  const excellonDrillOutput = stringifyExcellonDrill(excellon_drill_cmds)
+  const excellonDrillOutputUnplated = stringifyExcellonDrill(
+    excellon_drill_cmds_unplated,
+  )
 
-  expect(gerberOutput).toMatchGerberSnapshot(import.meta.path, "viagrid-board")
+  expect({
+    ...gerberOutput,
+    "drill.drl": excellonDrillOutput,
+    "drill_npth.drl": excellonDrillOutputUnplated,
+  }).toMatchGerberSnapshot(import.meta.path, "viagrid-board")
 })
