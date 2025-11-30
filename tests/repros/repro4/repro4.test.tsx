ab67a106e3961dad7145d9199403b884fbb48419
Severin Ibarluzea <seveibar@gmail.com>
2025-11-29 22:24:04 -0800

    Repro4 interconnect15x4 (#67)
    * repro4
    
    * reproduction of missing trace in gerber snapshot

---
+++ tests/repros/repro4/repro4.test.tsx
@@ -0,0 +1,32 @@
+import { test, expect } from "bun:test"
+import { convertSoupToGerberCommands } from "src/gerber/convert-soup-to-gerber-commands"
+import { stringifyGerberCommandLayers } from "src/gerber/stringify-gerber"
+import {
+  convertSoupToExcellonDrillCommands,
+  stringifyExcellonDrill,
+} from "src/excellon-drill"
+import viagridCircuitJson from "./repro4-interconnect15x4.json"
+
+test("Generate interconnect 15x4 board", async () => {
+  const gerber_cmds = convertSoupToGerberCommands(viagridCircuitJson as any)
+  const excellon_drill_cmds = convertSoupToExcellonDrillCommands({
+    circuitJson: viagridCircuitJson as any,
+    is_plated: true,
+  })
+  const excellon_drill_cmds_unplated = convertSoupToExcellonDrillCommands({
+    circuitJson: viagridCircuitJson as any,
+    is_plated: false,
+  })
+
+  const gerberOutput = stringifyGerberCommandLayers(gerber_cmds)
+  const excellonDrillOutput = stringifyExcellonDrill(excellon_drill_cmds)
+  const excellonDrillOutputUnplated = stringifyExcellonDrill(
+    excellon_drill_cmds_unplated,
+  )
+
+  expect({
+    ...gerberOutput,
+    "drill.drl": excellonDrillOutput,
+    "drill_npth.drl": excellonDrillOutputUnplated,
+  }).toMatchGerberSnapshot(import.meta.path, "interconnect15x4")
+})
