cfbcc36925fefa6fd0b3faef9d2b7eff94d0fb0e
AnasSarkiz <anassoftwaredev@gmail.com>
2026-05-21 13:06:18 +0200

    Add Native Gerber Aperture Support for Pill SMT Pads

---
+++ tests/repros/repro8/repro8.test.tsx
@@ -0,0 +1,40 @@
+import { test, expect } from "bun:test"
+import { convertSoupToGerberCommands } from "src/gerber/convert-soup-to-gerber-commands"
+import {
+  convertSoupToExcellonDrillCommands,
+  stringifyExcellonDrill,
+} from "src/excellon-drill"
+import { stringifyGerberCommandLayers } from "src/gerber/stringify-gerber"
+import { maybeOutputGerber } from "tests/fixtures/maybe-output-gerber"
+import blinkByddyJson from "./blinkByddy.json"
+import type { AnyCircuitElement } from "circuit-json"
+
+test("repro8 blinkByddy", async () => {
+  const circuitJson = blinkByddyJson as AnyCircuitElement[]
+
+  const gerberCmds = convertSoupToGerberCommands(circuitJson)
+  const excellonDrillCmdsPlated = convertSoupToExcellonDrillCommands({
+    circuitJson,
+    is_plated: true,
+  })
+  const excellonDrillCmdsUnplated = convertSoupToExcellonDrillCommands({
+    circuitJson,
+    is_plated: false,
+  })
+
+  const gerberOutput = stringifyGerberCommandLayers(gerberCmds)
+  const excellonDrillOutputPlated = stringifyExcellonDrill(
+    excellonDrillCmdsPlated,
+  )
+  const excellonDrillOutputUnplated = stringifyExcellonDrill(
+    excellonDrillCmdsUnplated,
+  )
+
+  await maybeOutputGerber(gerberOutput, excellonDrillOutputPlated)
+
+  expect({
+    ...gerberOutput,
+    "drill_plated.drl": excellonDrillOutputPlated,
+    "drill_unplated.drl": excellonDrillOutputUnplated,
+  }).toMatchGerberSnapshot(import.meta.path, "repro8-blinkByddy")
+})
