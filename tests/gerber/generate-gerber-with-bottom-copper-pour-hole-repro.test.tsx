55f607a95efb2126710075f448b7cab14b682f51
mohan-bee <mohn08052006@gmail.com>
2026-08-12 11:19:11 +0530

    up

--- tests/gerber/generate-gerber-with-bottom-copper-pour-hole-repro.test.tsx
+++ tests/gerber/generate-gerber-with-bottom-copper-pour-hole-repro.test.tsx
@@ -25,9 +25,16 @@ test("repro: bottom copper pour with centered hole", async () => {
     /%LPC\*%\nD\d+\*\nX000000000Y000000000D03\*\n%LPD\*%/,
   )
 
-  await expect(gerberOutput).toMatchGerberLayerOverlaySnapshot(
+  await expect(gerberOutput).toMatchGerberLayerSnapshots(
+    import.meta.path,
+    "bottom-copper-pour-hole-copper",
+    ["B_Cu"],
+  )
+
+  await expect(gerberOutput).toMatchCircuitJsonPcbAndGerberSnapshot(
     import.meta.path,
     "bottom-copper-pour-hole-repro",
+    circuitJson,
     ["B_Cu"],
   )
 })
